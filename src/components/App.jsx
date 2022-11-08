c3122bdb342fb3eda9ec402d58a985b1e95f92d8
DudaYura <YurkaD@ukr.net>
2022-11-08 11:16:57 +0200

    внес исправления

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,8 +12,9 @@ export const App = () => {
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ];
-  const parsedContacts = JSON.parse(window.localStorage.getItem('contacts'));
-  const [contacts, setContacts] = useState(parsedContacts ?? defaultContacts);
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) ?? defaultContacts
+  );
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
